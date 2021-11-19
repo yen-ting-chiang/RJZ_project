a282729c2f09d2bcb0a3f44460a4e4c8b548af95
yen-ting-chiang <dannyj114@gmail.com>
2021-11-19 02:04:26 -0600

    v1

---
+++ relaxed_gene_list_overlapping.R
@@ -0,0 +1,45 @@
+library(dplyr)
+setwd("C:/Users/dannyj/Documents/Rproject/RJZ_project/relaxed_gene_list")
+getwd()
+
+
+filenames  <- list.files(pattern = "*-MSC.anno_YTC.csv")
+
+gene_list <- purrr::map_df(filenames,
+                            ~read.csv(.x, stringsAsFactors = FALSE,
+                                      colClasses = "character") %>%
+                              filter(ExonicFunction %in% c("nonframeshift_substitution",
+                                                           "frameshift_substitution",
+                                                           "nonsynonymous_SNV",
+                                                           "stopgain",
+                                                           "stoploss")) %>% 
+                             distinct(Gene, .keep_all = FALSE) %>% 
+                              mutate(filename = .x))
+
+gene_number = gene_list %>% 
+  group_by(Gene) %>%
+  summarise(n())
+
+write.csv(gene_number, file = "gene_number.csv")
+
+combo_data <- purrr::map_df(filenames,
+                            ~read.csv(.x, stringsAsFactors = FALSE,
+                                      colClasses = "character") %>%
+                              filter(ExonicFunction %in% c("nonframeshift_substitution",
+                                                           "frameshift_substitution",
+                                                           "nonsynonymous_SNV",
+                                                           "stopgain",
+                                                           "stoploss")) %>% 
+                              mutate(filename = .x))
+
+write.csv(combo_data, 
+          file = "gene_list_combo_data.csv")
+
+
+overlap_3 = combo_data %>% 
+  filter(Gene %in% c("EPC2","FOXJ2","GABRR2","KCNMA1","LCORL",
+                     "LRP1B","MATR3","MUC6","PCDH7","USH2A","ZFHX3"))
+
+write.csv(overlap_3, 
+          file = "overlap_3_combo_data.csv")
+
