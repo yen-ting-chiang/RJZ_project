3df012d2cc7213f1b3effcc44d5fd8d1becd7167
yen-ting-chiang <dannyj114@gmail.com>
2022-01-03 01:40:09 -0600

    update ggthemes

--- mutation_type_figure.R
+++ mutation_type_figure.R
@@ -19,4 +19,41 @@ library(ggplot2)
 ggplot(data = combo_data_no_filter) + 
   geom_bar(aes(x = filename, 
                fill = RefGene),
-           position = "stack")
+           position = "stack",
+           alpha = 0.8) + 
+  scale_fill_manual(values = c("#a6cee3","#1f78b4","#b2df8a",
+                               "#33a02c","#fb9a99","#e31a1c",
+                               "#fdbf6f","#ff7f00","#cab2d6",
+                               "#6a3d9a","#ffff99","#b15928",
+                               "#543005")) + 
+  scale_color_manual(values = c("#a6cee3","#1f78b4","#b2df8a",
+                                "#33a02c","#fb9a99","#e31a1c",
+                                "#fdbf6f","#ff7f00","#cab2d6",
+                                "#6a3d9a","#ffff99","#b15928",
+                                "#543005")) +
+  theme_Publication() + 
+  theme(panel.border = 
+          element_rect(colour = "black", 
+                       fill = NA, 
+                       size = 1))
+
+ggplot(data = combo_data_no_filter) + 
+  geom_bar(aes(x = filename, 
+               fill = RefGene),
+           position = "fill",
+           alpha = 0.8) + 
+  scale_fill_manual(values = c("#a6cee3","#1f78b4","#b2df8a",
+                               "#33a02c","#fb9a99","#e31a1c",
+                               "#fdbf6f","#ff7f00","#cab2d6",
+                               "#6a3d9a","#ffff99","#b15928",
+                               "#543005")) + 
+  scale_color_manual(values = c("#a6cee3","#1f78b4","#b2df8a",
+                                "#33a02c","#fb9a99","#e31a1c",
+                                "#fdbf6f","#ff7f00","#cab2d6",
+                                "#6a3d9a","#ffff99","#b15928",
+                                "#543005")) +
+  theme_Publication() + 
+  theme(panel.border = 
+          element_rect(colour = "black", 
+                       fill = NA, 
+                       size = 1))
