81e8d132ee9999e797513bee16a72c046432bbc7
yen-ting-chiang <dannyj114@gmail.com>
2022-01-16 21:10:59 -0600

    complete

--- generate_world_cloud_by_allele_frequency.R
+++ generate_world_cloud_by_allele_frequency.R
@@ -2,13 +2,15 @@ setwd("C:/Users/dannyj/Documents/Rproject/RJZ_project/world_cloud_by_allele_freq
 getwd()
 library(dplyr)
 
+#generate dataframe containing word and frequency column------------------
 T2 <- read.csv(file = "LFS-T2-MSC.anno_YTC.csv")
 T2_filtered <- T2 %>% 
   filter(ExonicFunction %in% c("nonframeshift_substitution",
                                "frameshift_substitution",
                                "nonsynonymous_SNV",
                                "stopgain",
-                               "stoploss"))
+                               "stoploss")) %>% 
+  dplyr::filter(!grepl(",",Gene))
 T2_allele_frequency_sum <- 
   T2_filtered %>% 
   group_by(Gene) %>% 
@@ -23,7 +25,9 @@ T4_filtered <- T4 %>%
                                "frameshift_substitution",
                                "nonsynonymous_SNV",
                                "stopgain",
-                               "stoploss"))
+                               "stoploss")) %>% 
+  dplyr::filter(!grepl(",",Gene))
+
 T4_allele_frequency_sum <- 
   T4_filtered %>% 
   group_by(Gene) %>% 
@@ -38,7 +42,9 @@ T5_filtered <- T5 %>%
                                "frameshift_substitution",
                                "nonsynonymous_SNV",
                                "stopgain",
-                               "stoploss"))
+                               "stoploss")) %>% 
+  dplyr::filter(!grepl(",",Gene))
+
 T5_allele_frequency_sum <- 
   T5_filtered %>% 
   group_by(Gene) %>% 
@@ -47,17 +53,54 @@ T5_allele_frequency_sum <-
 write.csv(T5_allele_frequency_sum, file = "T5_allele_frequency_sum.csv")
 
 
+#generate wordcloud-------------------------------------------------
 
-install.packages("wordcloud")
+#install.packages("wordcloud")
 library(wordcloud)
-install.packages("RColorBrewer")
+#install.packages("RColorBrewer")
 library(RColorBrewer)
-install.packages("wordcloud2")
+#install.packages("wordcloud2")
 library(wordcloud2)
 
+# T2_allele_frequency_sum <- read.csv(file = "T2_allele_frequency_sum.csv")
+# T4_allele_frequency_sum <- read.csv(file = "T4_allele_frequency_sum.csv")
+# T5_allele_frequency_sum <- read.csv(file = "T5_allele_frequency_sum.csv")
 
-set.seed(1234) # for reproducibility
-wordcloud(words = df$word, freq = df$freq, min.freq = 1,
-          max.words=200, random.order=FALSE, rot.per=0.35,
-          colors=brewer.pal(8, "Dark2"))
+T2_allele_frequency_sum_top <- T2_allele_frequency_sum %>% 
+  head(30)
+wordcloud2(data = T2_allele_frequency_sum_top, 
+                      size = 0.5,
+                      fontFamily = 'Arial',
+                      shuffle = 'FALSE',
+                      color = 'random-dark',
+                      shape = 'circle')
 
+T4_allele_frequency_sum_top <- T4_allele_frequency_sum %>% 
+  head(30)
+wordcloud2(data = T4_allele_frequency_sum_top, 
+           size = 0.3,
+           fontFamily = 'Arial',
+           shuffle = 'FALSE',
+           color = 'random-dark',
+           shape = 'circle')
+
+
+T5_allele_frequency_sum_top <- T5_allele_frequency_sum %>% 
+  head(30)
+wordcloud2(data = T5_allele_frequency_sum_top, 
+           size = 0.5,
+           fontFamily = 'Arial',
+           shuffle = 'FALSE',
+           color = 'random-dark',
+           shape = 'circle')
+
+#wordcloud package-----------------------------------------------------------
+# set.seed(1234) # for reproducibility
+# wordcloud(words = T2_allele_frequency_sum$Gene,
+#           freq = T2_allele_frequency_sum$allele_frequency_sum,
+#           min.freq = 0,
+#           max.words = 30,
+#           scale = c(1.5,.15),
+#           random.order = FALSE,
+#           rot.per = 0,
+#           colors = brewer.pal(8, "Dark2"))
