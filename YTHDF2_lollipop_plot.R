da3e95ca1f5590dee68d1d24a182d4eb980438c6
yen-ting-chiang <dannyj114@gmail.com>
2022-08-18 23:05:20 -0500

    update

--- YTHDF2_lollipop_plot.R
+++ YTHDF2_lollipop_plot.R
@@ -5,8 +5,9 @@ if (!require("BiocManager", quietly = TRUE))
 
 BiocManager::install("trackViewer")
 library(trackViewer)
-YTHDF2_cosmic_mutation <- read.csv(file = "YTHDF2 mutaiton in COSMIC.csv")
 library(dplyr)
+
+YTHDF2_cosmic_mutation <- read.csv(file = "YTHDF2 mutaiton in COSMIC_2.csv")
 YTHDF2_cosmic_mutation <- YTHDF2_cosmic_mutation %>% 
   filter(Mutation.Type %in% c("Unknown","Substitution - coding silent") == FALSE)
 SNP <- as.numeric(YTHDF2_cosmic_mutation[,1])
@@ -17,17 +18,21 @@ features <- GRanges("chr1",
                                     width = c(100, 135),
                                     names = c("C1BD", "YTH")))
 
+sample.gr$SNPsideID <- "top"
+sample.gr$SNPsideID[35:41] <- "bottom"
+
+sample.gr$legend <- "COSMIC database"
+sample.gr$legend[35:41] <- "LFS-OBs-derived mouse tumors"
 
 features$fill <- c("#FF8833", "#51C6E6")
 sample.gr$color <- "#4C9900"
+sample.gr$color[35:41] <- "#0080FF"
 sample.gr$border <- "gray30"
 sample.gr$alpha <- 0.6
-features$height <- c(0.05, 0.05)
+features$height <- c(0.07, 0.05)
 sample.gr$score <- YTHDF2_cosmic_mutation[,5]
 xaxis <- c(1, 100, 200, 300, 400, 500, 579)
-yaxis <- c(0, 5, 10, 15)
-names(yaxis) <- yaxis # define the labels
-names(yaxis)[3] <- "y-axis" 
+yaxis <- c(0, 1, 2, 3)
 sample.gr$dashline.col <- "gray30"
 
 lolliplot(sample.gr, 
@@ -36,8 +41,10 @@ lolliplot(sample.gr,
           xaxis = xaxis,
           yaxis = FALSE,
           type="circle",
-          cex = 0.4,
+          cex = 0.8,
           jitter="label",
           ylab="Count",
           yaxis.gp = gpar(fontsize=5, lwd=0),
-          xaxis.gp = gpar(fontsize=7, lwd=0))
+          xaxis.gp = gpar(fontsize=10, lwd=0),
+          label_on_feature = TRUE,
+          legend="legend")
